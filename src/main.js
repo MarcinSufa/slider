92c8df35e66f783b0b35a8c9c84b4906054e6d75
MarcinSufa <sufa.marcin@gmail.com>
2020-05-22 16:24:30 +0200

    prepared scss and assets structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import addPicture from './script';
 import './styles/style.scss';
 
+import Swiper from 'swiper';
+
 document.addEventListener('DOMContentLoaded', function () {
     addPicture();
+
+    const galleryThumbs = new Swiper('.gallery-thumbs', {
+        // spaceBetween: 10,
+        // slidesPerView: 4,
+        // freeMode: true,
+        watchSlidesVisibility: true,
+        // watchSlidesProgress: true,
+    });
+    const galleryTop = new Swiper('.gallery-top', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+    });
 });
--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
 import sliderBcgImgUrl from './assets/slider-bcg.jpg';
+// import { Swiper, Navigation, Pagination, Scrollbar, EffectCoverflow } from 'swiper/dist/js/swiper.esm.js';
 
 export default function () {
-    const section = document.createElement('section');
-    const img = document.createElement('img');
-    img.src = sliderBcgImgUrl;
-    section.appendChild(img);
-    document.body.appendChild(section);
+    // const section = document.createElement('section');
+    // const img = document.createElement('img');
+    // img.src = sliderBcgImgUrl;
+    // section.appendChild(img);
+    // document.body.appendChild(section);
+    // const galleryThumbs = new Swiper('.gallery-thumbs', {
+    //     spaceBetween: 10,
+    //     slidesPerView: 4,
+    //     freeMode: true,
+    //     watchSlidesVisibility: true,
+    //     watchSlidesProgress: true,
+    // });
+    // const galleryTop = new Swiper('.gallery-top', {
+    //     spaceBetween: 10,
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    //     thumbs: {
+    //         swiper: galleryThumbs,
+    //     },
+    // });
 }
